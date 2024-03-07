e66f43af49c5bf34eea2dd64cb11277ebe0e5f1b
Agustinpc99 <Agustincasco99@hotmail.com>
2024-03-07 15:55:47 -0300

    v2.1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="flex px-2 bg-amber-500 py-3 items-center h-14"><p className='text-xl font-semibold'>V2.1</p></footer>
+    <footer className="flex px-2 bg-amber-500 py-3 items-center h-14 justify-center"><p className='text-xl font-semibold'>V2.1</p></footer>
   )
 }
 
