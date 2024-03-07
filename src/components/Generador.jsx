707e662f727026c39f341dd0d1dd1a62832e398c
Agustinpc99 <Agustincasco99@hotmail.com>
2024-03-06 22:38:22 -0300

    V2.1

--- src/components/Generador.jsx
+++ src/components/Generador.jsx
@@ -18,7 +18,7 @@ function Generador() {
             return sabores.legendario[seleccionSabor]
         }
     }
-    
+
     function crearPizza(){
         let pizzaNueva = []
         while(pizzaNueva.length < 4){
@@ -30,22 +30,32 @@ function Generador() {
         setPizza(pizzaNueva)
     }
 
-    
     if(!pizza.includes("")){
         let historialPizzas = JSON.parse(localStorage.getItem('historialPizzas')) || []
         historialPizzas.push(pizza)
         localStorage.setItem('historialPizzas', JSON.stringify(historialPizzas))
     }
     
+    let colores = []
+
+    for(let i = 0; i<4; i++){
+        if(sabores.comun.includes(pizza[i])){
+            colores.push("text-green-700")
+        } else if(sabores.raro.includes(pizza[i])){
+            colores.push("text-blue-800")
+        } else {
+            colores.push("text-red-800")
+        }
+    }
 
 
   return (
     <>
     <div className="h-[40vh] w-[90%] flex flex-col justify-center items-center bg-yellow-400 rounded-t text-xl gap-4 font-semibold md:h-[60vh] md:text-3xl lg:w-[80vw] xl:w-[60vw]">
-            <p>{pizza[0]}</p>
-            <p>{pizza[1]}</p>
-            <p>{pizza[2]}</p>
-            <p>{pizza[3]}</p>
+            <p className={colores[0]}>{pizza[0]}</p>
+            <p className={colores[1]}>{pizza[1]}</p>
+            <p className={colores[2]}>{pizza[2]}</p>
+            <p className={colores[3]}>{pizza[3]}</p>
           </div>
           <div className="w-[90%] flex flex-col justify-center items-center">
             <button onClick={crearPizza} className="bg-amber-400 w-full text-xl font-semibold rounded-b p-1 md:text-3xl lg:w-[80vw] xl:w-[60vw]">Generar Pizza</button>
--- src/views/History.jsx
+++ src/views/History.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import LayoutMain from '../components/LayoutMain'
+import {sabores} from "../data/sabores"
 
 function History() {
     let HistorialPizzas = JSON.parse(localStorage.getItem("historialPizzas")) || []
@@ -12,7 +13,15 @@ function History() {
             <p className='text-xl font-semibold'>Total: {pizzas.length}</p>
             <div className='grow flex flex-wrap gap-4 justify-center'>
                 {pizzas.length > 0 && pizzas.map((cards) => <div key={Math.random()} className="h-[300px] w-[300px] flex flex-col justify-center items-center bg-yellow-400 rounded-xl text-xl gap-4 font-semibold md:h-[350px] md:w-[350px] md:text-3xl border border-black">
-                {cards.length > 0 && cards.map((pizza) => <p key={Math.random()}>{pizza}</p>)}
+                {cards.length > 0 && cards.map((pizza) => {
+                  if(sabores.comun.includes(pizza)){ 
+                    return <p className='text-green-700' key={Math.random()}>{pizza}</p> 
+                  } else if (sabores.raro.includes(pizza)){
+                    return <p className='text-blue-800' key={Math.random()}>{pizza}</p>
+                  } else {
+                    return <p className='text-red-800' key={Math.random()}>{pizza}</p>
+                  }
+                  })}
                 </div> )}
           </div>
         </main>
