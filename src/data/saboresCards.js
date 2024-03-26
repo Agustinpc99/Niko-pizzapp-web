a652899a6a94a999c51442a39e95cfe176c4fd3a
Agustinpc99 <Agustincasco99@hotmail.com>
2024-03-26 10:20:11 -0300

    data cartas creada, seccion pizzas creada(faltan imagenes y descripciones)

---
+++ src/data/saboresCards.js
@@ -0,0 +1,352 @@
+export const cards = [
+    {
+        "imagen": "",
+        "nombre": "Cochina",
+        "descripcion": "",
+        "rareza": "comun"
+    },
+    {
+        "imagen": "",
+        "nombre": "Especial de rucula",
+        "descripcion": "",
+        "rareza": "comun"
+    },
+    {
+        "imagen": "",
+        "nombre": "Salchipapa",
+        "descripcion": "",
+        "rareza": "comun"
+    },
+    {
+        "imagen": "",
+        "nombre": "Provolone con panceta",
+        "descripcion": "",
+        "rareza": "comun"
+    },
+    {
+        "imagen": "",
+        "nombre": "Especial de pollo",
+        "descripcion": "",
+        "rareza": "comun"
+    },
+    {
+        "imagen": "",
+        "nombre": "Champignones",
+        "descripcion": "",
+        "rareza": "comun"
+    },
+    {
+        "imagen": "",
+        "nombre": "Especial de la casa",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Muzza",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Jamon y morron",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Jamon",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Morron",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Napolitana",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Napolitana c/jamon",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Napolitana c/huevo",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Napolitana c/Jamon y huevo",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Calabresa",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Palmitos",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Especial de palmitos",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Cuatro Quesos",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Especial de panceta",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Criolla",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Panceta c/Cheddar",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Especial de salchichas",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "De atun",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Muza al verdeo",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Rucula",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Berenjenas",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Especial de crudo",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Caprese",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Humita",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Choclo",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Primavera",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Primavera c/Jamon",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Correntina",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Verduras C/Salsa blanca",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Provolone",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Provolone c/jamon",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Roquefort",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Roquefort c/jamon",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Lluvia de papas",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Roquefort con panceta",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Margarita",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "A la bolognesa",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "De vegetales",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Fugazza con queso y panceta",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Fugazza con queso",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Fugazza cheddar",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Cheddar",
+        "descripcion": "",
+        "rareza": "raro"
+    },
+    {
+        "imagen": "",
+        "nombre": "Ananá",
+        "descripcion": "",
+        "rareza": "legendario"
+    },
+    {
+        "imagen": "",
+        "nombre": "Hawaiana",
+        "descripcion": "",
+        "rareza": "legendario"
+    },
+    {
+        "imagen": "",
+        "nombre": "Hawaiana c/crudo",
+        "descripcion": "",
+        "rareza": "legendario"
+    },
+    {
+        "imagen": "",
+        "nombre": "Romana",
+        "descripcion": "",
+        "rareza": "legendario"
+    },
+    {
+        "imagen": "",
+        "nombre": "Muzarella c/Anchoas",
+        "descripcion": "",
+        "rareza": "legendario"
+    },
+    {
+        "imagen": "",
+        "nombre": "Anchoas",
+        "descripcion": "",
+        "rareza": "legendario"
+    },
+    {
+        "imagen": "",
+        "nombre": "Mixta",
+        "descripcion": "",
+        "rareza": "legendario"
+    }
+]
+
+
+/* 
+    let pizzas = []
+    for(let i in sabores){
+        pizzas.push(sabores[i].map((sabor => [sabor,i])))
+    }
+
+    let pizzasCard = pizzas.flat().map((sabor) => {
+        return {
+            imagen : "",
+            nombre : sabor[0],
+            descripcion : "",
+            rareza : sabor[1]
+        }
+    })
+
+    console.log(pizzasCard)
+
+*/
--- src/views/Pizzas.jsx
+++ src/views/Pizzas.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { cards } from '../data/saboresCards'
 
 function Pizzas() {
+  let tarjetas = cards.map((tarjeta) => <div key={tarjeta.nombre} className="h-[300px] w-[300px] flex flex-col justify-center items-center bg-amber-500 rounded-xl text-xl gap-4 font-semibold md:h-[350px] md:w-[400px] md:text-3xl border border-black">
+    <img className='h-1/2' src="/pronto.png" alt="" />
+    <h2>{tarjeta.nombre}</h2>
+    <p>{tarjeta.descripcion}</p>
+    <span className={tarjeta.rareza == "comun" && "text-green-700" || tarjeta.rareza == "raro" && "text-blue-800" || tarjeta.rareza == "legendario" && "text-red-800" }>{tarjeta.rareza}</span>
+  </div>)
   return (
-    <div className='grow'>Pizzas</div>
+    <div className='grow flex flex-wrap gap-5 py-5 justify-center'>{tarjetas}</div>
   )
 }
 
